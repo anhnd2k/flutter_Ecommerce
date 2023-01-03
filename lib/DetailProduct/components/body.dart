96a5284df6a154084ddfce23955c8d048eceb55a
anhnh153 <anhnh153@viettel.com.vn>
2023-01-03 15:56:02 +0700

    detail product

--- lib/DetailProduct/components/body.dart
+++ lib/DetailProduct/components/body.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/Product.dart';
+import 'package:flutter_app/models/constants.dart';
 
 class BodyProduct extends StatelessWidget {
   final Product product;
+
   const BodyProduct({Key? key, required this.product}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-    )
+    Size size = MediaQuery
+        .of(context)
+        .size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: size.height * 0.3),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPaddin,
+                    right: kDefaultPaddin,
+                  ),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      )
+                  ),
+                  child: Text(product.title),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
